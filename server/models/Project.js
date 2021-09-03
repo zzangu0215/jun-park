a65708451a3a148c092373f4c1aeb91bf010454a
zzangu0215 <tajo0215@gmail.com>
2021-09-03 15:26:30 -0700

    Project Schema implemented

---
+++ server/models/Project.js
@@ -0,0 +1,40 @@
+const { Schema, model } = require("mongoose");
+
+const projectSchema = new Schema({
+  projectName: {
+    type: String,
+    required: "Project should have a unique name.",
+    trim: true,
+  },
+  thumbnail: {
+    type: String,
+    required: "Must have a thumbnail!",
+    trim: true,
+  },
+  description: {
+    type: String,
+    required: "Project should have a description.",
+    trim: true,
+    maxLength: 350,
+  },
+  github_link: {
+    type: String,
+    required: "Github link is required",
+    trim: true,
+  },
+  ext_link: {
+    type: String,
+    required: "Either youtube link or deployed link is needed.",
+    trim: true,
+  },
+  technologies: [
+    {
+      type: String,
+      trim: true,
+    },
+  ],
+});
+
+const Project = model("Project", projectSchema);
+
+module.exports = Project;
