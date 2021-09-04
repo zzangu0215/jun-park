646a3cced973aa40bb776819ddc83e6baa2ced05
Junny <tajo0215@gmail.com>
2021-09-04 11:35:23 -0700

    deleted unnecessary files from client/src and react router setup

---
+++ client/src/Main.js
@@ -0,0 +1,43 @@
+import React from "react";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+const httpLink = createHttpLink({
+  uri: "/graphql",
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("id_token");
+
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
+function Main() {
+  return (
+    <ApolloProvider client={client}>
+      <Router>
+        <Switch>
+          <Route exact path="/" />
+        </Switch>
+      </Router>
+    </ApolloProvider>
+  );
+}
+
+export default Main;
