3ee8dfb2332bfbab0a1e1e58e5018b93d2cab34f
zzangu0215 <tajo0215@gmail.com>
2021-09-04 15:12:52 -0700

    project cards custom made, but need to be fixed

--- client/src/components/Project-card/Project-card.js
+++ client/src/components/Project-card/Project-card.js
@@ -2,57 +2,17 @@ import React from "react";
 
 function ProjectCard() {
   return (
-    <div className="bg-gray-900 shadow-lg rounded p-3">
-      <div className="group relative">
+    <div className="lg:w-1/4 md:w-1/3 w-full lg:px-3 mt-6 m-4">
+      <div className="bg-gray-200 rounded-xl relative text-white">
         <img
-          className="w-full md:w-72 block rounded"
-          src="https://upload.wikimedia.org/wikipedia/en/f/f1/Tycho_-_Epoch.jpg"
-          alt=""
+          src="https://www.google.com/imgres?imgurl=https%3A%2F%2Fcdn.pixabay.com%2Fphoto%2F2015%2F04%2F23%2F22%2F00%2Ftree-736885__480.jpg&imgrefurl=https%3A%2F%2Fpixabay.com%2Fimages%2Fsearch%2Fsun%2F&tbnid=L8xfQakH9a8tJM&vet=12ahUKEwjXqLCKoObyAhWGtp4KHaQzBfkQMygAegUIARDKAQ..i&docid=T5n6IXZ5nFRelM&w=771&h=480&q=images&ved=2ahUKEwjXqLCKoObyAhWGtp4KHaQzBfkQMygAegUIARDKAQ"
+          alt="img"
+          className="object-cover h-96 w-full rounded-xl"
         />
-        <div className="absolute bg-black rounded bg-opacity-0 group-hover:bg-opacity-60 w-full h-full top-0 flex items-center group-hover:opacity-100 transition justify-evenly">
-          <button className="hover:scale-110 text-white opacity-0 transform translate-y-3 group-hover:translate-y-0 group-hover:opacity-100 transition">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="20"
-              height="20"
-              fill="currentColor"
-              className="bi bi-heart"
-              viewBox="0 0 16 16"
-            >
-              <path d="M8 2.748l-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z" />
-            </svg>
-          </button>
-
-          <button className="hover:scale-110 text-white opacity-0 transform translate-y-3 group-hover:translate-y-0 group-hover:opacity-100 transition">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="40"
-              height="40"
-              fill="currentColor"
-              className="bi bi-play-circle-fill"
-              viewBox="0 0 16 16"
-            >
-              <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM6.79 5.093A.5.5 0 0 0 6 5.5v5a.5.5 0 0 0 .79.407l3.5-2.5a.5.5 0 0 0 0-.814l-3.5-2.5z" />
-            </svg>
-          </button>
-
-          <button className="hover:scale-110 text-white opacity-0 transform translate-y-3 group-hover:translate-y-0 group-hover:opacity-100 transition">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="20"
-              height="20"
-              fill="currentColor"
-              className="bi bi-three-dots"
-              viewBox="0 0 16 16"
-            >
-              <path d="M3 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3z" />
-            </svg>
-          </button>
+        <div className="p-6 absolute bottom-0 left-0">
+          <h2 className="text-2xl font-bold mb-2">Card Title</h2>
+          <div className="leading-relaxed">Card body</div>
         </div>
-      </div>
-      <div className="p-5">
-        <h3 className="text-white text-lg">Epoch</h3>
-        <p className="text-gray-400">Tycho</p>
       </div>
     </div>
   );
--- client/src/pages/Projects/Projects.js
+++ client/src/pages/Projects/Projects.js
@@ -1,22 +1,16 @@
 import React from "react";
 import ProjectCard from "../../components/Project-card/Project-card";
+// import { FiChevronDown } from "react-icons/fi";
 
 function Projects() {
-  const style = {
-    marginTop: "100px",
-  };
-
   return (
     <>
       <h1>Projects</h1>
-      <div className="container mx-auto">
-        <div className="grid min-h-screen p-5" style={style}>
-          <section className="grid grid-cols-1 sm:grid-cols-3 gap-8">
-            <ProjectCard />
-            <ProjectCard />
-            <ProjectCard />
-          </section>
-        </div>
+      <div className="container mx-auto flex flex-wrap items-start justify-center my-16">
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
       </div>
     </>
   );
