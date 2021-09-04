e92c4023dc7bdaa7cd98b08e2e87fa317a15f24f
Junny <tajo0215@gmail.com>
2021-09-04 12:41:39 -0700

    added card design for projects page, but need to be fixed

--- client/src/pages/Projects/Projects.js
+++ client/src/pages/Projects/Projects.js
@@ -2,10 +2,23 @@ import React from "react";
 import ProjectCard from "../../components/Project-card/Project-card";
 
 function Projects() {
+  const style = {
+    marginTop: "100px",
+  };
+
   return (
-    <div>
-      <ProjectCard />
-    </div>
+    <>
+      <h1>Projects</h1>
+      <div className="container mx-auto">
+        <div className="grid min-h-screen p-5" style={style}>
+          <section className="grid grid-cols-1 sm:grid-cols-3 gap-8">
+            <ProjectCard />
+            <ProjectCard />
+            <ProjectCard />
+          </section>
+        </div>
+      </div>
+    </>
   );
 }
 
